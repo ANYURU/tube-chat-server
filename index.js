fee9abc7e87dc79ab0c544737760c658ba9c0051
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-04-03 12:11:15 +0300

    Allowed origins

--- index.js
+++ index.js
@@ -18,14 +18,7 @@ const allowedOrigins = [ "https://tube.ablestate.co", "https://tube-omega.vercel
 
 const io = new Server(server, {
     cors: {
-        origin: function( origin, callback) {
-            if( !origin ) return callback(null, true)
-            if (allowedOrigins.indexOf(origin) === -1) {
-                const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
+        origin: allowedOrigins,
         methods: ["GET", "POST"]
     },
 });
