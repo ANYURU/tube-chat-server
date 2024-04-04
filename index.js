17106d05372c21653e34781249f91aadaeb65b4a
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-04-04 12:32:31 +0300

    Allow multiple origins

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
-const cors  = require("cors");
-const PORT = process.env.PORT || 3001
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
@@ -14,60 +14,70 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const allowedOrigins = [ "https://tube.ablestate.co", "https://tube-omega.vercel.app"]
+const allowedOrigins = [
+  "https://tube.ablestate.africa",
+  "https://tube-omega.vercel.app",
+  "https://tube-gamma.vercel.app",
+];
 
 const io = new Server(server, {
-    cors: {
-        origin: allowedOrigins,
-        methods: ["GET", "POST"]
+  cors: {
+    origin: (origin, callback) => {
+      if (process.env.DEVELOPMENT && origin.startsWith("http://localhost")) {
+        return callback(null, true); 
+      }
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        return callback(null, true);
+      }
     },
+    methods: ["GET", "POST"],
+  },
 });
 
 io.use((socket, next) => {
-    const username = socket.handshake.auth.username;
-    socket.join(username)
-    
-    if (!username) {
-      return next(new Error("invalid username"));
-    }
-    socket.username = username;
-    next();
+  const username = socket.handshake.auth.username;
+  socket.join(username);
+
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
 });
 
 io.on("connection", (socket) => {
-    const users = []
+  const users = [];
 
-    for (let [ id, username] of io.of("/").sockets) {
-        users.push(username.username)
-    }
-    
-    io.of("/").emit("online_users", users)
+  for (let [, username] of io.of("/").sockets) {
+    users.push(username.username);
+  }
 
-    socket.on("check_online", (data) => {
-        socket.to(data).emit("online_users", users)
-    })
+  io.of("/").emit("online_users", users);
 
-    socket.on("send_message", ((data) => {
-        socket.to(data.receiver_id).emit("receive_message", data)
-    }))
+  socket.on("check_online", (data) => {
+    socket.to(data).emit("online_users", users);
+  });
 
-    socket.on("send_typing_status", (data) => {
-        console.log(data)
-        socket.to(data.receiver_id).emit("receive_typing_status", data)
-    })
-    
-    socket.on("logout", (data) => {
-        const index = users.indexOf(data)
-        if (index > -1) users.splice(index, 1)
-        io.of("/").in(data).local.disconnectSockets();
-        socket.broadcast.emit("user_disconnected", users)
-        console.log(`${data} is now offline`)
-        console.log("current online users: ", users)
-    })
-    
-})
+  socket.on("send_message", (data) => {
+    socket.to(data.receiver_id).emit("receive_message", data);
+  });
 
+  socket.on("send_typing_status", (data) => {
+    console.log(data);
+    socket.to(data.receiver_id).emit("receive_typing_status", data);
+  });
+
+  socket.on("logout", (data) => {
+    const index = users.indexOf(data);
+    if (index > -1) users.splice(index, 1);
+    io.of("/").in(data).local.disconnectSockets();
+    socket.broadcast.emit("user_disconnected", users);
+    console.log(`${data} is now offline`);
+    console.log("current online users: ", users);
+  });
+});
 
 server.listen(PORT, () => {
-    console.log(`server is running at port ${PORT}`)
-} )
+  console.log(`server is running at port ${PORT}`);
+});
