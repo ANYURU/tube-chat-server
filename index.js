35f4bb09de993380a1fff48260d596d28f95d2e0
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-04-04 15:16:32 +0300

    chore: add nodemon as a dev dependency

--- index.js
+++ index.js
@@ -24,14 +24,10 @@ const allowedOrigins = [
 const io = new Server(server, {
   cors: {
     origin: (origin, callback) => {
-      console.log("DEVELOPMENT: " ,process.env.DEVELOPMENT);
-      console.log("ORIGIN: ", origin);
 
       if (allowedOrigins.indexOf(origin) !== -1) {
-        console.log("ORIGIN ALLOWED: ", origin);
         return callback(null, true);
       } else {
-        console.log("ORIGIN NOT ALLOWED: ", origin);
         return callback(new Error("Not allowed by CORS"));
       }
     },
@@ -58,9 +54,10 @@ io.on("connection", (socket) => {
   }
 
   io.of("/").emit("online_users", users);
+  console.log("Online Users: ", users)
 
   socket.on("check_online", (data) => {
-    socket.to(data).emit("online_users", users);
+    console.log("Data: ", data)
   });
 
   socket.on("send_message", (data) => {
@@ -68,7 +65,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_typing_status", (data) => {
-    console.log(data);
     socket.to(data.receiver_id).emit("receive_typing_status", data);
   });
 
@@ -77,8 +73,6 @@ io.on("connection", (socket) => {
     if (index > -1) users.splice(index, 1);
     io.of("/").in(data).local.disconnectSockets();
     socket.broadcast.emit("user_disconnected", users);
-    console.log(`${data} is now offline`);
-    console.log("current online users: ", users);
   });
 });
 
